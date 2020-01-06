b2159f8bb5c511a7b5581e8ac25356ab13d4fcb5
Chamamou <49278313+Chamamou@users.noreply.github.com>
2020-01-06 18:11:05 +0100

    4.24

---
+++ RPG Project/Intermediate/Build/Mac/UE4Editor/Inc/Fallen/FallenGameMode.gen.cpp
@@ -0,0 +1,86 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "Fallen/FallenGameMode.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeFallenGameMode() {}
+// Cross Module References
+	FALLEN_API UClass* Z_Construct_UClass_AFallenGameMode_NoRegister();
+	FALLEN_API UClass* Z_Construct_UClass_AFallenGameMode();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_Fallen();
+// End Cross Module References
+	void AFallenGameMode::StaticRegisterNativesAFallenGameMode()
+	{
+	}
+	UClass* Z_Construct_UClass_AFallenGameMode_NoRegister()
+	{
+		return AFallenGameMode::StaticClass();
+	}
+	struct Z_Construct_UClass_AFallenGameMode_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AFallenGameMode_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_Fallen,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AFallenGameMode_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "FallenGameMode.h" },
+		{ "ModuleRelativePath", "FallenGameMode.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AFallenGameMode_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AFallenGameMode>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AFallenGameMode_Statics::ClassParams = {
+		&AFallenGameMode::StaticClass,
+		"Game",
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		UE_ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x008802ACu,
+		METADATA_PARAMS(Z_Construct_UClass_AFallenGameMode_Statics::Class_MetaDataParams, UE_ARRAY_COUNT(Z_Construct_UClass_AFallenGameMode_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AFallenGameMode()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AFallenGameMode_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AFallenGameMode, 3559463602);
+	template<> FALLEN_API UClass* StaticClass<AFallenGameMode>()
+	{
+		return AFallenGameMode::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AFallenGameMode(Z_Construct_UClass_AFallenGameMode, &AFallenGameMode::StaticClass, TEXT("/Script/Fallen"), TEXT("AFallenGameMode"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AFallenGameMode);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
